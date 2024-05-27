0647215c6c390f8fce21e115af18e5542d567e56
Constantin Denis <denis.io.constantin@gmail.com>
2024-05-28 00:05:13 +0300

    Add files via upload

---
+++ ex_6/ex_6.srcs/sources_1/new/program_counter.v
@@ -0,0 +1,15 @@
+module program_counter(
+    input clk,
+    input reset,
+    output reg [3:0] out
+);
+
+    always@(posedge clk) begin
+        if(reset)
+            out <= 0;
+        else
+            out <= out + 1;
+    end
+
+
+endmodule
